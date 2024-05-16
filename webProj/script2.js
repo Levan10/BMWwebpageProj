410aee51799395b90fe474cdd3b2993e106bab17
Levan10 <evan.lerman.473@my.csun.edu>
2024-05-15 19:35:45 -0700

    Small update to colors

--- webProj/script2.js
+++ webProj/script2.js
@@ -1,15 +1,11 @@
-// No need for the locations array and descriptions in JavaScript
-
 function createLocationElement(location) {
     const locationElement = document.createElement('div');
     locationElement.classList.add('location');
     locationElement.setAttribute('id', location.id);
 
     const img = document.createElement('img');
-    img.src = `${location.id}.avif`; // Assuming image filenames match location IDs
+    img.src = `${location.id}.avif`; 
     img.alt = location.name;
-
-    // No need to create location info here, as it's already in HTML
 
     locationElement.appendChild(img);
 
@@ -17,4 +13,4 @@ function createLocationElement(location) {
 }
 
 const map = document.querySelector('.map');
-// No need to iterate over locations and create elements here, as they're already in HTML
+
